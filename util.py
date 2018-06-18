818bfd83e6c58e6fa43b7e2873ed32b47c1ae2cf
Lily Smith <lillianjoycesmith@gmail.com>
2018-06-17 20:53:13 -0400

    Add files via upload

--- util.py
+++ util.py
@@ -10,7 +10,7 @@ class BrApp(QtWidgets.QMainWindow,br.Ui_MainWindow):
     def __init__(self):
         super(self.__class__, self).__init__()
         self.setupUi(self)
-        self.setWindowTitle("Bloomfield Robotics")
+        self.setWindowTitle("Image Display Window")
         self.setWindowIcon(QtGui.QIcon('br.png'))
 
         self.show()
